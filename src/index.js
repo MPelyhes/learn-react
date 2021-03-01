333990730a6b74fb017cf508edc7422d979eaebb
Matt Pelyhes <mattpelyhes@gmail.com>
2021-03-01 16:55:12 -0600

    use js variable in jsx

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
+  const buttonText = 'Touch My Button!';
+
   return (
    <div>
-    <label class="label" for="name">Enter name: </label>
+    <label className="label" for="name">Enter name: </label>
     <input id="name" type="text"/>
-    <button style={{backgroundColor: 'salmon', color: 'blue'}}>Touch Me</button>
+    <button style={{backgroundColor: 'salmon', color: 'blue'}}>{buttonText}</button>
   </div>
   );
 };
