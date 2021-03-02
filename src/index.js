bd05823fe86f792df0d213b16b96998badd20932
Matt Pelyhes <mattpelyhes@gmail.com>
2021-03-01 20:23:11 -0600

    wrap comments in approvalcard component

--- src/index.js
+++ src/index.js
@@ -2,30 +2,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import faker from "faker";
 
 // Create a react component
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Matt"
-        timeAgo="Today at 4:45PM"
-        commentText="Hey, this is great!"
-        imgSrc={faker.image.image()}
-      />
-      <CommentDetail
-        author="Gretchen"
-        timeAgo="Today at 2:13PM"
-        commentText="I love Chubblin and Chunklin"
-        imgSrc={faker.image.image()}
-      />
-      <CommentDetail
-        author="Chubblin"
-        timeAgo="Yesterday at 9:56PM"
-        commentText="Woof!"
-        imgSrc={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Matt"
+          timeAgo="Today at 4:45PM"
+          commentText="Hey, this is great!"
+          imgSrc={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Gretchen"
+          timeAgo="Today at 2:13PM"
+          commentText="I love Chubblin and Chunklin"
+          imgSrc={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Chubblin"
+          timeAgo="Yesterday at 9:56PM"
+          commentText="Woof!"
+          imgSrc={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
