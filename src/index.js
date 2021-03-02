5ae64700095bb42d13ab48db694e08267c0a0649
Matt Pelyhes <mattpelyhes@gmail.com>
2021-03-01 19:51:46 -0600

    add more props to comment

--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
 // Import the React and ReactDom Libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from "faker";
+import React from "react";
+import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 // Create a react component
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Matt"/>
-      <CommentDetail author="Gretchen"/>
-      <CommentDetail author="Chubblin"/>
-      <CommentDetail author="Bernie"/>
-      <CommentDetail author="Yeti"/>
+      <CommentDetail
+        author="Matt"
+        timeAgo="Today at 4:45PM"
+        commentText="Hey, this is great!"
+        imgSrc={faker.image.image()}
+      />
+      <CommentDetail
+        author="Gretchen"
+        timeAgo="Today at 2:13PM"
+        commentText="I love Chubblin and Chunklin"
+        imgSrc={faker.image.image()}
+      />
+      <CommentDetail
+        author="Chubblin"
+        timeAgo="Yesterday at 9:56PM"
+        commentText="Woof!"
+        imgSrc={faker.image.image()}
+      />
     </div>
   );
 };
 
 // Take the react component and show it on the screen
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
 
-if(module.hot){
+if (module.hot) {
   module.hot.accept();
 }
